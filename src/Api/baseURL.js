1101230448b0a2ac268515c8c593ff92eb99c7ab
MohamedElsayed <101253953+Mohamed215Elsayed@users.noreply.github.com>
2026-01-31 19:49:18 +0200

    deploy

--- src/Api/baseURL.js
+++ src/Api/baseURL.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const baseURL = axios.create({
-  baseURL: "https://eshop-api-project.vercel.app",
+  baseURL: "https://eshop-back-mu.vercel.app",
 //   baseURL: "http://127.0.0.1:8000",
     withCredentials: true, 
 });
 export default baseURL;
 
+// export default baseURL;
   // "start:prod": "set NODE_ENV=production&& node app.js",
